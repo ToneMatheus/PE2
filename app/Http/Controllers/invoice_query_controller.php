93b7b3b2928ab655bfff2d1b4bb8fcb9daef98e0
NikiDeVisscher <niki.de.visscher@gmail.com>
2024-03-07 16:05:35 +0100

    changed query for yearly invoices
    Changed the query to find all customers to invoice this month (yearly)

--- app/Http/Controllers/invoice_query_controller.php
+++ app/Http/Controllers/invoice_query_controller.php
@@ -10,13 +10,15 @@ class invoice_query_controller extends Controller
     public function contracts()
     {
 
-        $results1 = DB::select('select c.ID, c.customerID, c.startDate, c.endDate, c.type, c.price, c.status from customercontract c 
+        $results1 = DB::select('select c.ID AS cont_ID, c.customerID, c.startDate, c.endDate, c.type, c.price, c.status as cont_status, 
+        IFNULL(i.ID, \'NULL\' ) AS inv_ID 
+        from customercontract c 
         LEFT JOIN invoice i 
         ON c.ID = i.customerContractID
         WHERE MONTH(c.startDate) = MONTH(CURRENT_DATE())
         AND c.status LIKE \'active\'
         AND c.endDate > CURRENT_DATE()
-        AND i.status NOT LIKE \'paid\';');
+        AND i.ID IS NULL;');
 
         $resultsAll = DB::table('customercontract')->get();
 
