5a91f282c7aca95c9bc16c6e1c03488906d1adf6
SiebeVanHirtum <vanhirtumsiebe@gmail.com>
2024-03-07 14:55:49 +0100

    Updated meters-table.blade.php
    Added search functionality
    Added filter functionality
    Added pagination
    Added Item per page functionality

--- app/Models/Meter.php
+++ app/Models/Meter.php
@@ -9,4 +9,8 @@ class Meter extends Model
 {
     use HasFactory;
     public $table = "meter";
+
+    public function scopeSearch($query, $value){
+        $query->where('ID', 'like', "%{$value}%")->orWhere('EAN', 'like', "%{$value}%")->orWhere('type', 'like', "%{$value}%")->orWhere('installationDate', 'like', "%{$value}%")->orWhere('status', 'like', "%{$value}%");
+    }
 }
--- resources/views/livewire/meters-table.blade.php
+++ resources/views/livewire/meters-table.blade.php
@@ -1,20 +1,41 @@
 <div>
-    <table border="1px">
+    <div>
+        <select wire:model.live="type">
+            <option value="">All</option>
+            <option value="Electricity">Electricity</option>
+            <option value="Gas">Gas</option>
+        </select>
+        <select wire:model.live="status">
+            <option value="">All</option>
+            <option value="Installed">Installed</option>
+            <option value="In Storage">In Storage</option>
+        </select>
+    </div>
+    <input wire:model.live.debounce.300ms="search" type="text" placeholder="Search" style="border: 1px solid black">
+    <table class="border-collapse border border-slate-500">
         <tr>
-            <td>ID</td>
-            <td>EAN</td>
-            <td>Type</td>
-            <td>Installation Date</td>
-            <td>Status</td>
+            <td class="border border-slate-600">ID</td>
+            <td class="border border-slate-600">EAN</td>
+            <td class="border border-slate-600">Type</td>
+            <td class="border border-slate-600">Installation Date</td>
+            <td class="border border-slate-600">Status</td>
         </tr>
         @foreach ($meters as $meter)
             <tr>
-                <td>{{$meter->ID}}</td>
-                <td>{{$meter->EAN}}</td>
-                <td>{{$meter->type}}</td>
-                <td>{{$meter->installationDate}}</td>
-                <td>{{$meter->status}}</td>
+                <td class="border border-slate-700">{{$meter->ID}}</td>
+                <td class="border border-slate-700">{{$meter->EAN}}</td>
+                <td class="border border-slate-700">{{$meter->type}}</td>
+                <td class="border border-slate-700">{{$meter->installationDate}}</td>
+                <td class="border border-slate-700">{{$meter->status}}</td>
             </tr>    
         @endforeach
     </table>
+    <div>
+        <select wire:model.live="perPage">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="5">5</option>
+        </select>
+    </div>
+    {{$meters->links()}}
 </div>
