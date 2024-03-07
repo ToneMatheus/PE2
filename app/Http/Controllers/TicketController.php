19ca6da69f09e42eecc7a4be7cfa740af2505466
JasonDeckmyn <jason.deckmyn@hotmail.com>
2024-03-07 16:29:54 +0100

    Temp Ticket page

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+
+class TicketController extends Controller
+{
+    public function showForm(): View
+    {
+        return view('create-ticket');
+    }
+
+    // public function store(Request $request): Redirector|RedirectResponse
+    // {
+    //     $validatedData = $request->validate([
+    //         'cstID' => 'required',
+    //         'issue' => 'required|max:64',
+    //         'description' => 'required',
+    //     ]);
+
+    //     // create ticket
+    //     $ticket = new Ticket();
+    //     $ticket->cstID = $validatedData['cstID'];
+    //     $ticket->issue = $validatedData['issue'];
+    //     $ticket->description = $validatedData['description'];
+    //     $ticket->save();
+
+    //     return redirect()->route('show-ticket')->with(['ticket' => $ticket]);
+    // }
+
+    public function showSubmittedTicket(): View
+    {
+        $ticket = session('ticket');
+
+        return view('submitted-ticket', ['ticket' => $ticket]);
+    }
+}
