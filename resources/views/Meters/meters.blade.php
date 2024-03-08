e50d83fd6bd983a027554dab7f6c1fc73f1afde3
SiebeVanHirtum <vanhirtumsiebe@gmail.com>
2024-03-08 12:07:14 +0100

    Update meters.blade.php

--- resources/views/Meters/meters.blade.php
+++ resources/views/Meters/meters.blade.php
@@ -13,5 +13,25 @@
     <livewire:meters-table />
 
     @livewireScripts
+
+
+    <h1>EAN Number Test:</h1>
+<?php
+//https://stackoverflow.com/a/19890444
+function generateEAN($number)
+{
+  $code = '54' . str_pad($number, 15, '0', STR_PAD_LEFT);
+  $weightflag = true;
+  $sum = 0;
+  for ($i = strlen($code) - 1; $i >= 0; $i--)
+  {
+    $sum += (int)$code[$i] * ($weightflag?3:1);
+    $weightflag = !$weightflag;
+  }
+  $code .= (10 - ($sum % 10)) % 10;
+  return $code;
+}
+?>
+<h3>{{generateEAN(10)}}</h3>
 </body>
 </html>
