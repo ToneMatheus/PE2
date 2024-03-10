728acd663eb571099f3b1f4b383a1d386d36b1a0
ShaunyPersy <126152961+ShaunyPersy@users.noreply.github.com>
2024-03-10 15:06:05 +0100

    Changed Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,34 +13,34 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            //AddressSeeder::class,
-            //Employee\EmployeeProfileSeeder::class,
-            //UserSeeder::class,
-            //RoleSeeder::class,
-            //UserRoleSeeder::class,
-            //Employee\EmployeeContractSeeder::class,
-            //Employee\PayslipSeeder::class,
-            //Employee\HolidayTypeSeeder::class,
-            //Employee\BalanceSeeder::class,
-            //Employee\HolidaySeeder::class,
+            AddressSeeder::class,
+            Employee\EmployeeProfileSeeder::class,
+            UserSeeder::class,
+            RoleSeeder::class,
+            UserRoleSeeder::class,
+            Employee\EmployeeContractSeeder::class,
+            Employee\PayslipSeeder::class,
+            Employee\HolidayTypeSeeder::class,
+            Employee\BalanceSeeder::class,
+            Employee\HolidaySeeder::class,
 
-            //Customer\CustomerContractSeeder::class,
-            //Customer\CustomerAddressSeeder::class,
+            Customer\CustomerContractSeeder::class,
+            Customer\CustomerAddressSeeder::class,
 
-            //Meter\MeterSeeder::class,
-            //Meter\MeterAddressSeeder::class,
+            Meter\MeterSeeder::class,
+            Meter\MeterAddressSeeder::class,
 
-            //Invoice\EstimationSeeder::class,
-            //Invoice\TariffSeeder::class,
-            //Invoice\ProductSeeder::class,
-            //Invoice\ProductTariffSeeder::class,
-            //Invoice\ContractProductSeeder::class,
-            //Invoice\DiscountSeeder::class,
+            Invoice\EstimationSeeder::class,
+            Invoice\TariffSeeder::class,
+            Invoice\ProductSeeder::class,
+            Invoice\ProductTariffSeeder::class,
+            Invoice\ContractProductSeeder::class,
+            Invoice\DiscountSeeder::class,
 
-            //Meter\IndexValueSeeder::class,
-            //Meter\ConsumptionSeeder::class,
+            Meter\IndexValueSeeder::class,
+            Meter\ConsumptionSeeder::class,
 
-            //Invoice\InvoiceSeeder::class,
+            Invoice\InvoiceSeeder::class,
             Invoice\InvoiceLineSeeder::class,
         ]);
     }
