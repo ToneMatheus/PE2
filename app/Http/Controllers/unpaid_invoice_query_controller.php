35ca28e3d84efb4dd718c4c81abb6112e6c6f8b7
NikiDeVisscher <niki.de.visscher@gmail.com>
2024-03-12 10:21:13 +0100

    Update on unpaid invoice query

--- app/Http/Controllers/unpaid_invoice_query_controller.php
+++ app/Http/Controllers/unpaid_invoice_query_controller.php
@@ -12,7 +12,7 @@ class unpaid_invoice_query_controller extends Controller
         $resultsAll = DB::table('invoices')->get();
 
         $results1 = DB::select('SELECT * FROM invoices i
-                WHERE (i.status NOT LIKE \'paid\' AND i.status NOT LIKE \'draft\')
+                WHERE (i.status NOT LIKE \'paid\' AND i.status NOT LIKE \'pending\')
                 AND i.due_date < CURRENT_DATE();');
 
         return view("unpaid_invoice_query",['resultsAll'=>$resultsAll, 'results1'=>$results1]);
