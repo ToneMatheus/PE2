6dd0d9381173c6d665b594e7f46a2266fda8f5b0
JasonDeckmyn <jason.deckmyn@hotmail.com>
2024-03-14 16:52:01 +0100

    Added/Changed User, Address, Employee_Profile
    User:
    - Added validations for user data
    - Added missing fields after DB change
    - User model and Address is linked togehter
    To use it:
    // Create a new address for the user
            $address = new Address([
                'street' => $request->input('street'),
                'city' => $request->input('city'),
                'state' => $request->input('state'),
                'country' => $request->input('country'),
            ]);
    
            // Save the address for the user
            $user->addresses()->save($address);
    
    Address:
    - Added all the fields for address
    - Added validations
    
    Employee_Profile:
    - Added validations

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,58 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = 'user';
-    protected $primaryKey = 'ID';
+    public const VALIDATION_RULE_USERNAME = ['required'];
+    public const VALIDATION_RULE_FIRST_NAME = ['required'];
+    public const VALIDATION_RULE_LAST_NAME = ['required'];
+    public const VALIDATION_RULE_PASSWORD = ['required'];
+    public const VALIDATION_RULE_EMAIL = ['required'];
+    public const VALIDATION_RULE_PHONE_NBR = ['required'];
+    public const VALIDATION_RULE_BIRTHDATE = ['required'];
+
+    public const VALIDATION_RULES = [
+            'username' => self::VALIDATION_RULE_USERNAME,
+            'first_name' => self::VALIDATION_RULE_FIRST_NAME,
+            'last_name' => self::VALIDATION_RULE_LAST_NAME,
+            'password' => self::VALIDATION_RULE_PASSWORD,
+            'email' => self::VALIDATION_RULE_EMAIL,
+            'phone_nbr' => self::VALIDATION_RULE_PHONE_NBR,
+            'birth_date' => self::VALIDATION_RULE_BIRTHDATE,
+    ];
+
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
+    }
+
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+        'password',
+        'address_id',
+        'employee_profile_id',
+        'is_company',
+        'company_name',
+        'email',
+        'phone_nbr',
+        'birth_date',
+        'is_active'
+    ];
+
+    public static function validate(array $input): bool
+    {
+        $rules = static::VALIDATION_RULES;
+
+        return \Illuminate\Support\Facades\Validator::make($input, $rules)->passes();
+    }
 }
