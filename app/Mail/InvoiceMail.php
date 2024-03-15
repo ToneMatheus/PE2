1e7c98b56adfbfe0f0887f4174edc34f00662283
DeveloperFinn <finn-van-calster@hotmail.com>
2024-03-15 06:23:47 +0100

    Implementation of sending an email for invoices

---
+++ app/Mail/InvoiceMail.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Invoice;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class InvoiceMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(protected Invoice $invoice, protected $name)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address('energysupplier@gmail.com', 'Energy Supplier'),
+            /* It is possible to allow a GLOBAL email to be used. This is useful if we only use 1 email to send out all emails. 
+            'from' => [
+                'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+                'name' => env('MAIL_FROM_NAME', 'Example'),
+            ],
+            */
+            subject: 'Invoice Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.invoice_mail',  
+            with: [
+                'name' => $this->name,
+                'invoiceType' => $this->invoice->type,
+                'invoicePrice' => $this->invoice->price,
+                'invoiceStatus' => $this->invoice->status,
+                'invoiceDueDate' => $this->invoice->due_date,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
