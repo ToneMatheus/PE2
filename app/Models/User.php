0ea48697145a8ea5e2076c5c67ebf29e95430a7f
Tone Matheus <r0947882@student.thomasmore.be>
2024-03-15 11:39:34 +0100

    Fixed duplicate

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,10 +61,7 @@ class User extends Authenticatable
         return \Illuminate\Support\Facades\Validator::make($input, $rules)->passes();
     }
     
-    public function addresses()
-    {
-        return $this->hasMany(Address::class);
-    }
+ 
     public function creditNotes()
     {
         return $this->hasMany(CreditNote::class);
