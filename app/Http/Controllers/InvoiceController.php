c7e58b5b42456e01b5a1eced78f96fb9469276c3
DeveloperFinn <finn-van-calster@hotmail.com>
2024-03-15 07:07:29 +0100

    Implementation for downloading invoice as Pdf

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\InvoiceMail;
+use App\Models\Invoice;
+use App\Models\Invoice_line;
+use App\Models\User;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+
+class InvoiceController extends Controller
+{
+    //
+    public function sendMail(Request $request)
+    {
+        $invoice = Invoice::where('id', $request->id)->first();
+        $user = User::where('id', $invoice->user_id)->first();
+        if ($invoice != null) {
+            //finnvc99@gmail.com is going to be replaced with: $user->email
+            Mail::to('finnvc99@gmail.com')->send(new InvoiceMail($invoice, $user->name));
+        }
+        return redirect()->intended('dashboard');
+    }
+
+    public function download(Request $request)
+    {
+        $invoice = Invoice::where('id', $request->id)->first();
+        $pdf = Pdf::loadView('Invoices.invoice_pdf', compact('invoice'));
+        return $pdf->download('invoice.pdf');
+    }
+}
