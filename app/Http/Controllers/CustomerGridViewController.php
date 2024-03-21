7404cebd6f04c92c1ab29c095bb629ac3e1c7ae7
AlexanderMertens2002 <r0842789@student.thomasmore.be>
2024-03-21 16:29:14 +0100

    removed leftover from testing

--- app/Http/Controllers/CustomerGridViewController.php
+++ app/Http/Controllers/CustomerGridViewController.php
@@ -22,7 +22,7 @@ class CustomerGridViewController extends Controller
     $direction = $request->get('direction', 'asc');
     $search = $request->get('search');
     $query = DB::table('users')
-                ->leftjoin('customer_contracts', 'users.id', '=', 'customer_contracts.user_id')
+                ->join('customer_contracts', 'users.id', '=', 'customer_contracts.user_id')
                 ->select('users.id', 'users.username', 'users.first_name', 'users.last_name', 'users.phone_nbr', 'users.is_company', 'users.company_name', 'users.email', 'users.birth_date', 'users.is_activate', 'customer_contracts.start_date', 'customer_contracts.end_date', 'customer_contracts.type', 'customer_contracts.price', 'customer_contracts.status');
 
         if ($search) {
