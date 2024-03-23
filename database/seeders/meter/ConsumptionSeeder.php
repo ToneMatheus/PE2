9689bea3670447c8dfe9136ec11e49f975234652
ShaunyPersy <126152961+ShaunyPersy@users.noreply.github.com>
2024-03-23 11:32:05 +0100

    Seeder conflict solved

--- database/seeders/meter/ConsumptionSeeder.php
+++ database/seeders/meter/ConsumptionSeeder.php
@@ -9,72 +9,33 @@ class ConsumptionSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('consumptions')->insert([
-            [
-                'start_date' => '2024-01-01',
-                'end_date' => '2024-02-01',
-                'consumption_value' => 35,
-                'prev_index_id' => 1,
-                'current_index_id' => 2 
-            ],
-            [
-                'start_date' => '2024-02-01',
-                'end_date' => '2024-03-01',
-                'consumption_value' => 30,
-                'prev_index_id' => 2,
-                'current_index_id' => 3 
-            ]
-        ]);
+        for($i = 1; $i <= 4; $i++){
+            DB::table('consumptions')->insert([
+                'start_date' => '2022-01-01',
+                'end_date' => '2023-01-01',
+                'consumption_value' => 0,
+                'prev_index_id' => null,
+                'current_index_id' => $i
+            ]);
+        }
+        
+        for($i = 1; $i <= 4; $i++){
+            $indexValues = DB::table('index_values')->where('meter_id', '=', $i)->get();
 
-        DB::table('consumptions')->insert([
-            [
-                'start_date' => '2024-01-01',
-                'end_date' => '2024-02-01',
-                'consumption_value' => 40,
-                'prev_index_id' => 4,
-                'current_index_id' => 5 
-            ],
-            [
-                'start_date' => '2024-02-01',
-                'end_date' => '2024-03-01',
-                'consumption_value' => 120,
-                'prev_index_id' => 5,
-                'current_index_id' => 6
-            ]
-        ]);
-
-        DB::table('consumptions')->insert([
-            [
-                'start_date' => '2024-01-01',
-                'end_date' => '2024-02-01',
-                'consumption_value' => 75,
-                'prev_index_id' => 7,
-                'current_index_id' => 8 
-            ],
-            [
-                'start_date' => '2024-02-01',
-                'end_date' => '2024-03-01',
-                'consumption_value' => 30,
-                'prev_index_id' => 8,
-                'current_index_id' => 9
-            ]
-        ]);
-
-        DB::table('consumptions')->insert([
-            [
-                'start_date' => '2024-01-01',
-                'end_date' => '2024-02-01',
-                'consumption_value' => 15,
-                'prev_index_id' => 10,
-                'current_index_id' => 11
-            ],
-            [
-                'start_date' => '2024-02-01',
-                'end_date' => '2024-03-01',
-                'consumption_value' => 10,
-                'prev_index_id' =>11,
-                'current_index_id' => 12
-            ]
-        ]);
+            foreach($indexValues as $key => $indexValue){
+                if ($key > 0) {
+                    $prevIndexValue = $indexValues[$key - 1];
+                    $consumptionValue = $indexValue->reading_value - $prevIndexValue->reading_value;
+    
+                    DB::table('consumptions')->insert([
+                        'start_date' => $prevIndexValue->reading_date,
+                        'end_date' => $indexValue->reading_date,
+                        'consumption_value' => $consumptionValue,
+                        'prev_index_id' => $prevIndexValue->id,
+                        'current_index_id' => $indexValue->id,
+                    ]);
+                }
+            }
+        }
     }
 }
