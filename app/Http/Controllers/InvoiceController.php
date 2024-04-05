d100a18f864c308e34b066e5aaa260acda436b70
Roland Moons <r0831610@student.thomasmore.be>
2024-04-05 14:49:28 +0200

    Added lost function
    a function went missing in the invoice controller which a paga required to work

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -149,4 +149,48 @@ class InvoiceController extends Controller
     {
         return view('Invoices.CustomerInvoicesOverview');
     }
+
+    public function showInvoices(Request $request){
+
+        // This fetch is dumb as it gets all invoices just to show a dynamic year filter
+        // But i cant rely on the invoices that go through the query as that may have filters on there
+        $invoices = Invoice::all();
+        $filterYears = [];
+            foreach($invoices as $invoice){
+                $invoiceYear = date('Y', strtotime($invoice['InvoiceDate']));
+                if (!in_array($invoiceYear, $filterYears)){
+                    array_push($filterYears, $invoiceYear);
+                }
+            }        
+
+        $invoicesQuery = Invoice::query();
+
+        // Filter by time range
+        $selectedYear = "";
+        if ($request->has('year') && $request->input('year') != '') {
+            $selectedYear = $request->input('year');
+            if ($selectedYear === 'last3Months') {
+                $invoicesQuery->whereDate('invoiceDate', '>=', now()->subMonths(3));
+            } elseif ($selectedYear === 'last6Months') {
+                $invoicesQuery->whereDate('invoiceDate', '>=', now()->subMonths(6));
+            } else {
+                $invoicesQuery->whereYear('invoiceDate', $selectedYear);
+            }
+        }
+
+        // Filter by payment status
+        $selectedStatus = "";
+        if ($request->has('status') && $request->input('status') != '') {
+            $selectedStatus = $request->input('status');
+            if ($selectedStatus === 'Paid') {
+                $invoicesQuery->where('status', 'Paid');
+            } elseif ($selectedStatus === 'Unpaid') {
+                $invoicesQuery->where('status', 'Unpaid');
+            }
+        }
+
+        $invoices = $invoicesQuery->get();
+
+        return view('Invoices/CustomerInvoicesOverview', compact('invoices', 'filterYears', 'selectedYear', 'selectedStatus'));
+    }
 }
