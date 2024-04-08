5fa54050dd6a16516dd6d60337cf7a1dcd43553b
ShaunyPersy <126152961+ShaunyPersy@users.noreply.github.com>
2024-04-08 11:31:22 +0200

    fixed seeders

--- database/seeders/employee/EmployeeProfileSeeder.php
+++ database/seeders/employee/EmployeeProfileSeeder.php
@@ -28,38 +28,26 @@ class EmployeeProfileSeeder extends Seeder
             [   
                 'id' => 4,
                 'hire_date' => '2024-01-01',
-                'work_email' => 'kd3@example.com',
-                'password' => Hash::make('joseph')
             ],
             [   
                 'id' => 5,
                 'hire_date' => '2024-01-01',
-                'work_email' => 'kd3@example.com',
-                'password' => Hash::make('jimmy')
             ],
             [   
                 'id' => 6,
                 'hire_date' => '2024-01-01',
-                'work_email' => 'kd3@example.com',
-                'password' => Hash::make('james')
             ],
             [   
                 'id' => 7,
                 'hire_date' => '2024-01-01',
-                'work_email' => 'kd3@example.com',
-                'password' => Hash::make('jane')
             ],
             [   
                 'id' => 8,
                 'hire_date' => '2024-01-01',
-                'work_email' => 'kd3@example.com',
-                'password' => Hash::make('pearl')
             ],
             [   
                 'id' => 9,
                 'hire_date' => '2024-01-01',
-                'work_email' => 'kd3@example.com',
-                'password' => Hash::make('rose')
             ],
         ]);
     }
