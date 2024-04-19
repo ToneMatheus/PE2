a57efdfd1ab5e5fa3af38c587ba8f4f86822ed59
SiebeVanHirtum <vanhirtumsiebe@gmail.com>
2024-04-19 10:59:02 +0200

    Fixes before demo

--- resources/views/Meters/Meter_History.blade.php
+++ resources/views/Meters/Meter_History.blade.php
@@ -1,4 +1,3 @@
-<x-app-layout>
     <!DOCTYPE html>
     <html>
     <head>
@@ -34,6 +33,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
+        
         <h1>Meter History</h1>
         @if (count($errors) > 0)
             <div class="alert alert-danger">
@@ -119,5 +119,4 @@
     </html>
 
 
-</x-app-layout>
 
