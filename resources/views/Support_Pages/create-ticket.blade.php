ffdfc37442682d474f5e4ef4d245b3293eda72ac
JasonDeckmyn <jason.deckmyn@hotmail.com>
2024-04-21 14:34:07 +0200

    Page title change
    Implemented change of the title page

--- resources/views/Support_Pages/create-ticket.blade.php
+++ resources/views/Support_Pages/create-ticket.blade.php
@@ -1,9 +1,8 @@
-{{-- -*-html-*- --}}
-{{-- <x-app-layout> --}}
-  <title>Ticket Support</title>
-    <div>
-
-      <form method="POST" action="{{ route('submitted-ticket') }}">
+<x-app-layout :title="$title">
+  <div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8 bg-white dark:bg-gray-800 p-10 rounded-lg shadow-md">
+      <h1 class="text-2xl font-bold text-center dark:text-white underline mb-6">Create Ticket</h1>
+      <form method="POST" action="{{ route('submitted-ticket') }}" class="mt-8 space-y-6">
         @csrf
         <label for="name">Name</label>
         <input type=text name="name" id="name"
@@ -23,4 +22,4 @@
         <button type=submit>Submit</button>
       </form>
     </div>
-{{-- </x-app-layout> --}}
+</x-app-layout>
