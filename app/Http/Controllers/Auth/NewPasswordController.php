8156a4a6d719c06de64df17bb6cd37662dfa3324
ShaunyPersy <126152961+ShaunyPersy@users.noreply.github.com>
2024-04-22 11:28:41 +0200

    Employee reset default password

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use App\Models\User;
 
 class NewPasswordController extends Controller
 {
@@ -27,7 +28,7 @@ class NewPasswordController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
         $request->validate([
             'token' => ['required'],
@@ -35,27 +36,20 @@ class NewPasswordController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user) use ($request) {
-                $user->forceFill([
-                    'password' => Hash::make($request->password),
-                    'remember_token' => Str::random(60),
-                ])->save();
+        $user = User::where('email', '=', $request->email)
+        ->first();
 
-                event(new PasswordReset($user));
-            }
-        );
+        if($user && $user->remember_token === $request->token){
+            $user->password = Hash::make($request->password);
+            $user->remember_token = null;
+            $user->changed_default = 1;
+            $user->save();
 
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
-        return $status == Password::PASSWORD_RESET
-                    ? redirect()->route('login')->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+            event(new PasswordReset($user));
+
+            return redirect()->route('login')->with('status', 'Your password has been reset successfully. Please log in with your new password.');
+        }
+
+        return redirect()->route('password.reset', ['token' => $request->token])->withErrors(['email' => 'Invalid email or token. Please try again.']);
     }
 }
--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class PasswordResetLinkController extends Controller
 {
@@ -23,22 +25,23 @@ class PasswordResetLinkController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
         $request->validate([
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+        $user = User::where('email', '=', $request->email)
+        ->first();
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+        if(!is_null($user)){
+            $token = Str::random(64);
+
+            $user->remember_token = $token;
+            $user->sendPasswordResetNotification($token);
+            $user->save();
+        }
+
+        return back()->with('status', 'If there is a user with this email, you will receive a mail shortly.');
     }
 }
