734f45babc5a6e2dd17d73d24aaae294645d2cbb
Fax-spittah <eddytiger7@gmail.com>
2024-05-11 09:16:39 +0200

    Some  minor changes

--- resources/views/intranet/employeeIntranet.blade.php
+++ resources/views/intranet/employeeIntranet.blade.php
@@ -4,10 +4,24 @@
     }
 </style>
 
+<script>
+    function check(index) {
+        var checkBox = document.getElementById('checkBox' + index);
+        var value = document.getElementById('check' + index);
+        var valueText = value.innerText;
+
+        if (checkBox.checked) {
+            value.innerHTML = "<del>" + valueText + "</del>";
+        } else {
+            value.innerText = valueText;
+        }
+    }
+</script>
+
 <div class="intranet" style="margin: 20px 30px 30px 0px; display: flex; justify-content: space-between;">
     <div class="left-side">
-        <div class="card" style="display: flex; width: 600px; border-radius: 5px">
-            <img class="card-img" src="/images/tropical-vacation.jpg" alt="Card image" style="width: 40%; border-top-left-radius: 5px; border-bottom-left-radius: 5px;">
+        <div class="card" style="display: flex; width: 800px; border-radius: 5px; height: 180px">
+            <img class="card-img" src="/images/tropical-vacation.jpg" alt="Card image" style="width: 35%; border-top-left-radius: 5px; border-bottom-left-radius: 5px;">
             <div class="card-body" style="background-color: rgb(242, 240, 240); padding: 35px; border-top-right-radius: 5px; border-bottom-right-radius: 5px;">
               <h4 class="card-title h4" style=" font-weight: 900; font-size: 18px">Planning to take time off?</h4>
               <p class="card-text text-sm" style="padding: 8px 0px;">Check out all these amazing destinations where most of our employees travel to</p>
@@ -15,17 +29,17 @@
             </div>
         </div>
 
-        <div class="card" style="display: flex; width: 600px; max-width: 600px; border-radius: 5px; margin-top: 20px;">
-            <img class="card-img" src="/images/greenpowerimage.jpg" alt="Card image" style="width: 40%; border-top-left-radius: 5px; border-bottom-left-radius: 5px;">
+        <div class="card" style="display: flex; width: 800px; border-radius: 5px; margin-top: 20px; height: 180px">
+            <img class="card-img" src="/images/greenpowerimage.jpg" alt="Card image" style="width: 35%; border-top-left-radius: 5px; border-bottom-left-radius: 5px;">
             <div class="card-body" style="background-color: rgb(242, 240, 240); padding: 35px; border-top-right-radius: 5px; border-bottom-right-radius: 5px;">
               <h4 class="card-title h4" style=" font-weight: 900; font-size: 18px">Heading towards green energy</h4>
-              <p class="card-text text-sm" style="padding: 8px 0px;">Our company's energy to be 100% green by 2035</p>
+              <p class="card-text text-sm" style="padding: 8px 0px;">Our company's energy to be 100% green by 2035. What is this green energy?</p>
               <a href="https://www.nationalgrid.com/stories/energy-explained/what-is-green-energy" class="text-blue-500">Read more</a>
             </div>
         </div>
 
-        <div class="card" style="display: flex; width: 600px; max-width: 600px; border-radius: 5px; margin-top: 20px;">
-            <img class="card-img" src="/images/care.png" alt="Card image" style="width: 40%; border-top-left-radius: 5px; border-bottom-left-radius: 5px;">
+        <div class="card" style="display: flex; width: 800px; border-radius: 5px; margin-top: 20px; height: 180px">
+            <img class="card-img" src="/images/care.png" alt="Card image" style="width: 35%; border-top-left-radius: 5px; border-bottom-left-radius: 5px;">
             <div class="card-body" style="background-color: rgb(242, 240, 240); padding: 35px; border-top-right-radius: 5px; border-bottom-right-radius: 5px;">
               <h4 class="card-title h4" style=" font-weight: 900; font-size: 18px">Benefits and compensation</h4>
               <p class="card-text text-sm" style="padding: 8px 0px;">We care about our workers. See the many benefits we provide as a company</p>
@@ -35,24 +49,41 @@
     </div>
     
     <a href="#" class="block">
-        <div class="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-700 rounded-lg shadow p-4" style="margin-left: 230px">
-            <span class="text-black-700 hover:text-blue-700 dark:text-white dark:hover:text-gray-400 mb-2" style="font-size: 18px; font-weight: 900;">To-do</span>
-            <p class="text-gray-600 dark:text-gray-400 text-sm">        
-                <ul style="width: 300px;">
-                <li>Take out trash</li>
-                <li>Update customer information</li>
-                <li>Clean office</li>
-            </ul></p>
+        <div class="tw=block flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-700 rounded-lg shadow p-4" style="margin-left: 40px">
+            <span class="text-black-700 hover:text-blue-700 dark:text-white dark:hover:text-gray-400 mb-2" style="font-size: 18px; font-weight: 900;">Daily tasks</span>
+            <ul class="list-group" style="width: 280px">
+                <li class="list-group-item">
+                    <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." id="checkBox1" onclick="check(1)">
+                    <span id="check1">First checkbox</span>
+                </li>
+                <li class="list-group-item">
+                  <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." id="checkBox2" onclick="check(2)">
+                  <span id="check2">Second checkbox</span>
+                </li>
+                <li class="list-group-item">
+                  <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." id="checkBox3" onclick="check(3)">
+                  <span id="check2">Third checkbox</span>
+                </li>
+                <li class="list-group-item">
+                  <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." id="checkBox4" onclick="check(4)">
+                  <span id="check2">Fourth checkbox</span>
+                </li>
+                <li class="list-group-item">
+                  <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." id="checkBox5" onclick="check(5)">
+                  <span id="check2">Fifth checkbox</span>
+                </li>
+              </ul>
+              
+              <div style="display: flex">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Add task
+                  </button>
+                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Clear tasks
+                  </button>
+              </div>
         </div>
     </a>
 
-    <div class="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-700 rounded-lg shadow p-4" style="margin-left: 230px">
-        <span class="text-black-700 hover:text-blue-700 dark:text-white dark:hover:text-gray-400 mb-2" style="font-size: 18px; font-weight: 900;">New employees</span>
-        <p class="text-gray-600 dark:text-gray-400 text-sm">        
-            <ul style="width: 300px;" class="list-none md:list-disc">
-            <li>Take out trash</li>
-            <li>Update customer information</li>
-            <li>Clean office</li>
-        </ul></p>
-    </div>
+    
 </div>
