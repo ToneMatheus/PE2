a92a2be2b6dd1135d169593b4e801b29339b76f3
ShaunyPersy <126152961+ShaunyPersy@users.noreply.github.com>
2024-05-16 18:06:54 +0200

    Updated seeders

--- database/seeders/meter/IndexValueSeeder.php
+++ database/seeders/meter/IndexValueSeeder.php
@@ -13,72 +13,145 @@ class IndexValueSeeder extends Seeder
         DB::table('index_values')->insert([
             [
                 'id' => 1,
+                'reading_date' => '2023-01-01',
+                'reading_value' => 3200,
+                'meter_id' => 1
+            ],
+            [
+                'id' => 2,
+                'reading_date' => '2023-01-01',
+                'reading_value' => 6500,
+                'meter_id' => 2
+            ],
+            [
+                'id' => 3,
+                'reading_date' => '2023-01-01',
+                'reading_value' => 3300,
+                'meter_id' => 3
+            ],
+            [
+                'id' => 4,
+                'reading_date' => '2023-01-01',
+                'reading_value' => 3400,
+                'meter_id' => 4
+            ],
+            [
+                'id' => 5,
+                'reading_date' => '2024-01-01',
+                'reading_value' => 3300,
+                'meter_id' => 1
+            ],
+            [
+                'id' => 6,
+                'reading_date' => '2024-01-01',
+                'reading_value' => 6600,
+                'meter_id' => 2
+            ],
+            [
+                'id' => 7,
+                'reading_date' => '2024-01-01',
+                'reading_value' => 3400,
+                'meter_id' => 3
+            ],
+            [
+                'id' => 8,
+                'reading_date' => '2024-01-01',
+                'reading_value' => 3200,
+                'meter_id' => 4
+            ],
+            [
+                'id' => 9,
+                'reading_date' => '2025-01-01',
+                'reading_value' => 3400,
+                'meter_id' => 1
+            ],
+            [
+                'id' => 10,
+                'reading_date' => '2025-01-01',
+                'reading_value' => 6700,
+                'meter_id' => 2
+            ],
+            [
+                'id' => 11,
+                'reading_date' => '2025-01-01',
+                'reading_value' => 3500,
+                'meter_id' => 3
+            ],
+            [
+                'id' => 12,
+                'reading_date' => '2025-01-01',
+                'reading_value' => 3100,
+                'meter_id' => 4
+            ],
+        
+            [
+                'id' => 13,
                 'reading_date' => '2024-01-01',
                 'reading_value' => 900,
                 'meter_id' => 9
             ],
             [
-                'id' => 2,
+                'id' => 14,
                 'reading_date' => '2024-02-01',
                 'reading_value' => 1800,
                 'meter_id' => 9
             ],
             [
-                'id' => 3,
+                'id' => 15,
                 'reading_date' => '2024-03-01',
                 'reading_value' => 2700,
                 'meter_id' => 9
             ],
             [
-                'id' => 4,
+                'id' => 16,
                 'reading_date' => '2024-04-01',
                 'reading_value' => 3600,
                 'meter_id' => 9
             ],
             [
-                'id' => 5,
+                'id' => 17,
                 'reading_date' => '2024-05-01',
                 'reading_value' => 4500,
                 'meter_id' => 9
             ],
             [
-                'id' => 6,
+                'id' => 18,
                 'reading_date' => '2024-06-01',
                 'reading_value' => 5400,
                 'meter_id' => 9
             ],
             [
-                'id' => 7,
+                'id' => 19,
                 'reading_date' => '2024-07-01',
                 'reading_value' => 6300,
                 'meter_id' => 9
             ],
             [
-                'id' => 8,
+                'id' => 20,
                 'reading_date' => '2024-08-01',
                 'reading_value' => 7200,
                 'meter_id' => 9
             ],
             [
-                'id' => 9,
+                'id' => 21,
                 'reading_date' => '2024-09-01',
                 'reading_value' => 8100,
                 'meter_id' => 9
             ],
             [
-                'id' => 10,
+                'id' => 22,
                 'reading_date' => '2024-10-01',
                 'reading_value' => 9000,
                 'meter_id' => 9
             ],
             [
-                'id' => 11,
+                'id' => 23,
                 'reading_date' => '2024-11-01',
                 'reading_value' => 9900,
                 'meter_id' => 9
             ],
             [
-                'id' => 12,
+                'id' => 24,
                 'reading_date' => '2024-12-01',
                 'reading_value' => 10800,
                 'meter_id' => 9
