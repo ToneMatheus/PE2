6ea434c634fa2e85136b36a3f90ae82a80729d11
AlexanderMertens2002 <r0842789@student.thomasmore.be>
2024-05-17 10:18:10 +0200

    quick fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,7 @@
                                 <p class="text-gray-600 dark:text-gray-400 text-sm">View all tickets</p>
                             </div>
                         </a>
-                        <a href="{{ route('Support_Pages.flowchart.Flowchart-ascalade-ticket') }}" class="block">
+                        <a href="{{ route('Support_Pages.flowchart.Flowchart-ascalade-ticket2') }}" class="block">
                             <div class="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-700 rounded-lg shadow p-4">
                                 <span class="text-blue-500 hover:text-blue-700 dark:text-white dark:hover:text-gray-400 mb-2">Flowchart</span>
                                 <p class="text-gray-600 dark:text-gray-400 text-sm">Flowchart for ticket escalation</p>
