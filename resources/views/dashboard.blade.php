0676610a5d31e6a50c84070843b17fedb70fd083
JasonDeckmyn <jason.deckmyn@hotmail.com>
2024-05-21 10:32:33 +0200

    Removed old template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -146,12 +146,7 @@
                             
                     @endif
                     @if($roleId == config('roles.BOSS'))
-                        <a href="{{ route('submitted-ticket') }}" class="block">
-                            <div class="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-700 rounded-lg shadow p-4">
-                                <span class="text-blue-500 hover:text-blue-700 dark:text-white dark:hover:text-gray-400 mb-2">Submitted Tickets</span>
-                                <p class="text-gray-600 dark:text-gray-400 text-sm">View your submitted tickets</p>
-                            </div>
-                        </a>
+                       
                     @endif
                     @if($roleId == config('roles.FINANCE_ANALYST'))
                         <a href="{{ route('show-ticket') }}" class="block">
