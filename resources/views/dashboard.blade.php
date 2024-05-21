cd0c8d3890d6b5bb4527de3afbff50ebdbee44f7
SeghersZeno <r0940253@student.thomasmore.be>
2024-05-21 13:48:48 +0200

    added ticket dashboard to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -209,6 +209,16 @@
                         </a>
                     @endif 
                     @if($roleId == config('roles.EMPLOYEE'))
+                        {{-- for the helpdesk to see the ticket dashboard --}}
+                        <a href="{{ route('ticket_dashboard') }}" class="block">
+                            <div class="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-700 rounded-lg shadow p-4">
+                                <span class="text-blue-500 hover:text-blue-700 dark:text-white dark:hover:text-gray-400 mb-2">Ticket dashboard</span>
+                                <p class="text-gray-600 dark:text-gray-400 text-sm">View everything about the tickets</p>
+                            </div>
+                        </a>
+
+
+
                         {{-- for the employees to manage their holiday requests --}}
                         <a href="{{ route('request') }}" class="block">
                             <div class="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-700 rounded-lg shadow p-4">
@@ -292,12 +302,12 @@
                     @endif
 
                     @if($roleId == config('roles.CUSTOMER_SERVICE'))
-                        <a href="{{ route('ticket_dashboard') }}" class="block">
+                        <!-- <a href="{{ route('ticket_dashboard') }}" class="block">
                             <div class="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-700 rounded-lg shadow p-4">
                                 <span class="text-blue-500 hover:text-blue-700 dark:text-white dark:hover:text-gray-400 mb-2">Ticket dashboard</span>
                                 <p class="text-gray-600 dark:text-gray-400 text-sm">View everything about the tickets</p>
                             </div>
-                        </a>
+                        </a> -->
                     @endif
 
                     @if($roleId == config('roles.CUSTOMER'))
