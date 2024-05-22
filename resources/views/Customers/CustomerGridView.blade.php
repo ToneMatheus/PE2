8a5ea0fcfe1e2d081a59f6688cb98bfbace37294
YannickStrackx <126255102+YannickStrackx@users.noreply.github.com>
2024-05-22 16:46:17 +0200

    remove commented section

--- resources/views/Customers/CustomerGridView.blade.php
+++ resources/views/Customers/CustomerGridView.blade.php
@@ -7,60 +7,6 @@
     </h2>
 </x-slot>
 
-
-<!-- <div class="container">
-    <div class="row">
-        <form id="searchForm">
-            <input type="text" name="search" id="searchInput" required value="{{ request('search') }}">
-            <button type="submit">Search</button>
-        </form>
-        <a href="{{ route('customerGridView') }}" class="reset-button">Clear</a>
-    </div>
-    <table id="customerTable">
-        <thead>
-            <th><a href="#" class="sort" data-column="id">ID</a></th>
-            <th><a href="#" class="sort" data-column="last_name">Last Name</a></th>
-            <th><a href="#" class="sort" data-column="first_name">First Name</a></th>
-            <th><a href="#" class="sort" data-column="phone_nbr">Phone Number</a></th>
-            <th><a href="#" class="sort" data-column="company_name">Company Name</a></th>
-            <th><a href="#" class="sort" data-column="is_company">Is Company</a></th>
-            <th><a href="#" class="sort" data-column="email">Email</a></th>
-            <th><a href="#" class="sort" data-column="birth_date">Birth Date</a></th>
-            <th><a href="#" class="sort" data-column="is_active">Is Active</a></th>
-            <th><a href="#" class="sort" data-column="start_date">Start Date</a></th>
-            <th><a href="#" class="sort" data-column="end_date">End Date</a></th>
-            <th><a href="#" class="sort" data-column="type">Type</a></th>
-            <th><a href="#" class="sort" data-column="price">Price</a></th>
-            <th><a href="#" class="sort" data-column="status">Status</a></th>
-            <th>Edit</th>
-            <th>Add Invoice Line</th>
-        </thead>
-        <tbody>
-            @foreach ($customers as $customer)
-                <tr>
-                    <td>{{ $customer->id }}</td>
-                    <td>{{ $customer->last_name }}</td>
-                    <td>{{ $customer->first_name }}</td>
-                    <td>{{ $customer->phone_nbr }}</td>
-                    <td>{{ $customer->company_name }}</td>
-                    <td>{{ $customer->is_company }}</td>
-                    <td>{{ $customer->email }}</td>
-                    <td>{{ $customer->birth_date }}</td>
-                    <td>{{ $customer->is_active }}</td>
-                    <td>{{ $customer->start_date }}</td>
-                    <td>{{ $customer->end_date }}</td>
-                    <td>{{ $customer->type }}</td>
-                    <td>{{ $customer->price }}</td>
-                    <td>{{ $customer->status }}</td>
-                    <td><a href="{{ route('customer.edit', ['id' => $customer->id]) }}">Edit</a></td>
-                    <td><a href="{{ route('addInvoiceExtraForm', ['id' => $customer->id]) }}">Add Invoice Line</a></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $customers->links() }}
-</div> -->
-
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
