97ea225befabb8438fe8217148add48409ec652e
VanCalsterFinn <95765255+VanCalsterFinn@users.noreply.github.com>
2024-05-23 12:15:46 +0200

    Migration tweak

--- database/migrations/2024_03_15_064855_add_credit_notes.php
+++ database/migrations/2024_03_15_064855_add_credit_notes.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->boolean('status');
             $table->text('description')->nullable();
             $table->double('amount')->nullable(); 
-            $table->foreignId('invoice_id')->references('id')->on('invoices')->onDelete('cascade')->nullable();
+            $table->foreignId('invoice_id')->nullable();
             $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
